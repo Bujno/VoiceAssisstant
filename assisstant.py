2c8bea32c54be0df2dba790c378ade765d1e71d9
Bujno <0k.bujnowicz@gmail.com>
2021-02-27 12:34:38 +0100

    Create make note option

--- assisstant.py
+++ assisstant.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import datetime
 import pickle
-import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -10,10 +9,12 @@ import pytz
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
+import subprocess
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 MONTHS = ["january", "february", "march", "april", "may", "june","july", "august", "september","october", "november", "december"]
 DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
 
 def speak(text):
     '''
@@ -24,6 +25,7 @@ def speak(text):
     filename = 'voice.mp3'
     tts.save(filename)
     playsound.playsound(filename)
+    os.remove("voice.mp3")
     
     
 def get_audio():
@@ -36,14 +38,12 @@ def get_audio():
     with sr.Microphone() as source:
         audio = r.listen(source)
         said = ""
-
         try:
             said = r.recognize_google(audio)
             print(said)
         except Exception as e:
-            print("Exception: " + str(e))
-
-    return said
+            print(str(e))
+    return said.lower()
 
 
 def authenticate_google():
@@ -54,7 +54,6 @@ def authenticate_google():
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -62,10 +61,8 @@ def authenticate_google():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
-
     return build('calendar', 'v3', credentials=creds)
 
 
@@ -104,7 +101,6 @@ def get_date(text):
     '''
     text = text.lower()
     today = datetime.date.today()
-
     if text.count("today") > 0:
         return today
 
@@ -123,34 +119,51 @@ def get_date(text):
 
     if month < today.month and month != -1:
         year = year+1
-
     if month == -1 and day != -1:  
         month = today.month + 1 if day < today.day else today.month
-
     if month == -1 and day == -1 and day_of_week != -1:
         current_day_of_week = today.weekday()
         dif = day_of_week - current_day_of_week
-
         if dif < 0:
             dif += 7
             if text.count("next") >= 1:
                 dif += 7
-
         return today + datetime.timedelta(dif)
-
     if day != -1:  
         return datetime.date(month=month, day=day, year=year)
     
     
+def note(text):
+    date = datetime.datetime.now()
+    file_name = str(date).replace(":", "-") + "-note.txt"
+    with open(file_name, "w") as f:
+        f.write(text)
+    subprocess.Popen(["notepad.exe", file_name])
+    
+    
+    
 SERVICE = authenticate_google()
 print("Start")
-text = get_audio()
 
 CALENDAR_STRS = ["what do i have", "do i have plans", "am i busy"]
-for phrase in CALENDAR_STRS:
-    if phrase in text.lower():
-        date = get_date(text)
-        if date:
-            get_events(date, SERVICE)
-        else:
-            speak("Please Try Again")
+NOTE_STRS = ["make a note", "write this down", "remember this", "type this"]
+
+while True:
+    print("Listening")
+    text = get_audio()
+    if text.count('wake up') > 0:
+        speak("I am ready")
+        text = get_audio()
+        for phrase in CALENDAR_STRS:
+            if phrase in text:
+                date = get_date(text)
+                if date:
+                    get_events(date, SERVICE)
+                else:
+                    speak("Please Try Again")     
+        for phrase in NOTE_STRS:
+            if phrase in text:
+                speak("What would you like me to write down? ")
+                write_down = get_audio()
+                note(write_down)
+                speak("I've made a note of that.")
